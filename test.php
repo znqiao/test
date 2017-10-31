95145262a7ee4e5880c2fa9dc11c73d2d2493f73
zengyq <zengyq@greatwall.com.cn>
2017-10-31 14:37:12 +0800

    second commit

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var_dump('0x'.strtoupper(dechex(0b111111111111)));
+
